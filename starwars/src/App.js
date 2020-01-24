25df2f190aa957e87ac3a4ddc27699b422af109f
Draxxus702 <58435416+KennithHowe2@users.noreply.github.com>
2020-01-24 11:58:33 -0800

    not done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+
+import Card from './components/Card'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,32 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+const [star, starState] = useState([])
+
+useEffect(()=>{
+  axios.get('https://swapi.co/api/people/')
+  .then(response =>{
+    starState(response.data.results)
+    console.log(response.data.results)
+  })
+  .catch(error =>{
+    console.log('Could not retrieve Data', error)
+  })
+}, [])
+
   return (
+    
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      {console.log('this is a star',star)}
+       {/* <h1 className="Header">React Wars</h1> */}
+{/* //       {star.results.map(p => (  */}
+{/* //     <Card */}
+{/* //     key={p.edited}
+//     name={p.name}
+//     /> */}
+     
+   
+{/* // ))} */}
     </div>
   );
 }
---
+++ starwars/src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './StarWars.css'
+
+function Card(props){
+console.log('help', props)
+return (
+<div>
+
+
+
+  
+</div>
+)
+      }
+export default Card;
+
+
+
